28b3d9da1dc4d5da7588bc0cfb8dd6728168d180
Felix Wegener <Felix.Wegener05@t-online.de>
2023-07-05 15:42:36 +0200

    add awsome fonts

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -3,6 +3,9 @@ import React, { useEffect, useState } from 'react';
 import ReactDOM from "react-dom";
 import { useNavigate } from "react-router-dom";
 import { getHumid, getTemp } from '../../utils/Api/TempSensorApi';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowsRotate, faBars, faCloud, faTemperatureThreeQuarters } from '@fortawesome/free-solid-svg-icons';
+import { icon } from '@fortawesome/fontawesome-svg-core';
 
 export const MainPage = () => {
     const navigation = useNavigate();
@@ -10,6 +13,13 @@ export const MainPage = () => {
     const [currenthumidity, setCurrenthumidity] = useState<number>();
 
     let isMounted = true;
+
+    const info = async () => {
+        if (isMounted) {
+            setCurrentTemp(await getTemp());
+            setCurrenthumidity(await getHumid());
+        }
+    }
 
     useEffect(() => {
         const info = async () => {
@@ -39,18 +49,33 @@ export const MainPage = () => {
         >
             <VStack>
                 <Box
-                rounded={'sm'}
+                    rounded={'sm'}
                     backgroundColor='white'
                     width={'50%'}
+                    alignItems={'flex-start'}
+                    paddingLeft={8}
                 >
-                    <Button onFocus={() => navigation('/license')} backgroundColor={'white'}>
-                        <HStack space={6}>
-                            <HamburgerIcon />
+                    <Button onFocus={() => navigation('/license')} backgroundColor={'white'} paddingLeft={-2}>
+                        <HStack space={4}>
+                            <FontAwesomeIcon icon={faBars} />
                             <Text>Lizenzen</Text>
                         </HStack>
                     </Button>
-                    <Text>Aktuelle Temperatur: {currentTemp}°C</Text>
-                    <Text>Aktuelle Luftfeuchtigkeit: {currenthumidity}%</Text>
+                    <HStack space={4}>
+                        <FontAwesomeIcon icon={faTemperatureThreeQuarters} />
+                        <Text>{currentTemp}°C</Text>
+                    </HStack>
+                    <HStack space={4}>
+                        <FontAwesomeIcon icon={faCloud} />
+                        <Text>{currenthumidity}%</Text>
+                    </HStack>
+                    
+                    <Button backgroundColor={'white'} onFocus={() => info()} paddingLeft={-2}>
+                        <HStack space={4}>
+                            <FontAwesomeIcon icon={faArrowsRotate} />
+                            <Text>Refresh Temperature</Text>
+                        </HStack>
+                    </Button>
                     <View height={"50px"}/>
                 </Box>
             </VStack>
